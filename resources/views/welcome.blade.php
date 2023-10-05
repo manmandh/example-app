88938c0b5ba5acdea7a809d0322ffbf13737089b
manmandh <mantm.22ite@vku.udn.vn>
2023-10-05 14:55:12 +0700

    #1 Fix: setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <title>{{ config('app.name', 'vinasupport.com') }}</title>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Scripts -->
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
